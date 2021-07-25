1bd869841b3be4b23379da166568dff2080b1306
Lucas Neves <lucas54neves@gmail.com>
2021-07-25 13:12:05 -0300

    add course and review serializers

---
+++ src/courses/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Course, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Review
+        fields = (
+            'id',
+            'course',
+            'name',
+            'comment',
+            'rating',
+            'created_at',
+            'updated_at',
+            'active'
+        )
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            'id',
+            'title',
+            'url',
+            'created_at',
+            'updated_at',
+            'active'
+        )
