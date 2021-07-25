8039c1468df54101b479498bb1dd7149ac04d864
Lucas Neves <lucas54neves@gmail.com>
2021-07-25 18:53:53 -0300

    add course and review views

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Course, Review
+from .serializers import CourseSerializer, ReviewSerializer
+
+class CourseAPIView(APIView):
+    """
+    Courses API
+    """
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
+
+class ReviewAPIView(APIView):
+    """
+    Reviews API
+    """
+    def get(self, request):
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
