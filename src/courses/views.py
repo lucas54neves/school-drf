2e77820016920609955f8957d54ad1c08192aa81
Lucas Neves <lucas54neves@gmail.com>
2021-07-25 19:59:20 -0300

    add post method for courses and reviews

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Course, Review
 from .serializers import CourseSerializer, ReviewSerializer
@@ -12,6 +13,12 @@ class CourseAPIView(APIView):
         courses = Course.objects.all()
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ReviewAPIView(APIView):
     """
@@ -21,3 +28,9 @@ class ReviewAPIView(APIView):
         reviews = Review.objects.all()
         serializer = ReviewSerializer(reviews, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ReviewAPIView(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
