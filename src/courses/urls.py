867c593639e93a546de688bd69a567e91770236d
Lucas Neves <lucas54neves@gmail.com>
2021-08-19 12:13:48 -0300

    update urls

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import CourseAPIView, ReviewAPIView
+from .views import (
+    CoursesAPIView,
+    ReviewsAPIView,
+    CourseAPIView,
+    ReviewAPIView,
+    CourseViewSet,
+    ReviewViewSet
+)
+
+router = SimpleRouter()
+
+router.register('courses', CourseViewSet)
+router.register('reviews', ReviewViewSet)
 
 urlpatterns = [
-    path('courses/', CourseAPIView.as_view(), name='courses'),
-    path('reviews/', ReviewAPIView.as_view(), name='reviews')
+    path('courses/', CoursesAPIView.as_view(), name='courses'),
+    path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'),
+    path('courses/<int:course_pk>/reviews', ReviewsAPIView.as_view(), name='course_reviews'),
+    path('courses/<int:course_pk>/reviews/<int:review_pk>', ReviewAPIView.as_view(), name='course_review'),
+    path('reviews/', ReviewsAPIView.as_view(), name='reviews'),
+    path('reviews/<int:review_pk>/', ReviewAPIView.as_view(), name='review')
 ]
